825d44cf93bcfb82ec9c35cfc828f22ff9bf9450
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-09 19:52:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,11 +8,11 @@ class Song
   attr_accessor :name
   attr_reader :artist
 
-  @@songs = []
+  # @@songs = []
 
-  def initialize
-    @@songs << self
-  end
+  # def initialize
+  #   # @@songs << self
+  # end
 
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
